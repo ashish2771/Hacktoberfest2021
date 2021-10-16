08bb3b2da7ebef0eb0aad080e885950081f1cde5
ashish2771 <56157565+ashish2771@users.noreply.github.com>
2021-10-16 09:37:42 +0530

    Add files via upload

---
+++ moving_data_around.m
@@ -0,0 +1,28 @@
+%% dimensions
+sz = size(A) % 1x2 matrix: [(number of rows) (number of columns)]
+size(A,1) % number of rows
+size(A,2) % number of cols
+length(v) % size of longest dimension
+
+
+load q1y.dat   % alternatively, load('q1y.dat')
+load q1x.dat
+who   % list variables in workspace
+whos  % list variables in workspace (detailed view) 
+
+%% indexing
+A(3,2)  % indexing is (row,col)
+A(2,:)  % get the 2nd row. 
+        % ":" means every element along that dimension
+A(:,2)  % get the 2nd col
+A([1 3],:) % print all  the elements of rows 1 and 3
+
+A(:,2) = [10; 11; 12]     % change second column
+A = [A, [100; 101; 102]]; % append column vec
+A(:) % Select all elements as a column vector.
+
+% Putting data together 
+A = [1 2; 3 4; 5 6]
+B = [11 12; 13 14; 15 16] % same dims as A
+C = [A B]  % concatenating A and B matrices side by side
+C = [A; B] % Concatenating A and B top and bottom
